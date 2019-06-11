a17b1c698a543078d41cca0ab2bb940a84e93b82
LucasM <lucasmgs21@gmail.com>
2019-06-11 00:40:26 -0300

    Adiciona variaveis ambiente

--- src/models/File.js
+++ src/models/File.js
@@ -16,7 +16,8 @@ const File = new mongoose.Schema({
 });
 
 File.virtual('url').get(function(){
-    return `http://localhost:3333/files/${encodeURIComponent(this.path)}`
+    const url = process.env.url ||  'http://localhost:3333/files/';
+    return `${url}/{encodeURIComponent(this.path)}`
 });
 
 module.exports = mongoose.model('File', File);
